88b9691741fc5db39a74571c41c51a42b55fe052
sanyaches <sanyaches95@gmail.com>
2023-09-02 18:25:31 +0200

    Add double-check plugin for set auth token

---
+++ plugins/authPlugin.client.ts
@@ -0,0 +1,14 @@
+import { defineNuxtPlugin } from '@nuxtjs/composition-api'
+import { accessTokenKey } from '@/modules/auth/constants'
+import { useAuthorizeHeader } from "@/modules/shared/infrastructure/api/useAuthorizeHeader"
+
+export default defineNuxtPlugin(({ $cookies }) => {
+  const { setAuthorizationHeader } = useAuthorizeHeader()
+
+  const token = $cookies.get(accessTokenKey)
+  if (!token) {
+    return
+  }
+
+  setAuthorizationHeader(token)
+})
